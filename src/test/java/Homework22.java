a56177cb78929c0476a40cd86c9417dfa348710e
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-01-24 23:07:36 -0600

    Create Homework22.java

---
+++ src/test/java/Homework22.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+import pages.AllSongsPage;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class Homework22 extends BaseTest {
+
+
+
+    @Test
+    public void reNamePlaylist () throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        AllSongsPage allsongspage = new AllSongsPage(driver);
+
+
+        loginPage.logIn();
+        allsongspage.clickAllSongspage();
+        allsongspage.getAllSongsTitle();
+
+        Thread.sleep(2000);
+
+    }
+
+}
