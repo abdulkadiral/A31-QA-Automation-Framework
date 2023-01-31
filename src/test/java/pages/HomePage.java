943ed98d784aab4703792de91558cfa9a4a0d1dd
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-01-31 09:05:29 -0600

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,10 +3,14 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
 
+    @FindBy
     By userAvatarIcon = By.cssSelector("img.avatar");
+
+    By searchField = By.cssSelector("[type=\"search\"]");
 
     public HomePage( WebDriver givenDriver) {
         super(givenDriver);
@@ -14,6 +18,13 @@ public class HomePage extends BasePage {
 
     public WebElement getUserAvatar () {
         return findElement(userAvatarIcon);
+    }
+    public void enterinSearchField(String item){
+        WebElement searchBox = driver.findElement(searchField);
+        searchBox.click();
+        searchBox.sendKeys(item);
+
+
     }
 
 }
