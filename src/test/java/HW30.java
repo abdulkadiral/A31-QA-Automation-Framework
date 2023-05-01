c689d4c5eb7562c85ae764e9c68b365d7f16fcb7
abdulkadiral <121259833+abdulkadiral@users.noreply.github.com>
2023-05-01 16:34:51 -0500

    Create HW30.java

---
+++ src/test/java/HW30.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class HW30 extends BaseTest{
+
+
+    @Test
+    public void loginNew (){
+        login("adeagle2021@gmail.com","te$t$tudent");
+    }
+}
